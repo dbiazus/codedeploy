fb84bb474dfd4beab3be025a9f45b3c15babad95
Daniel Biazus <picollib@gmail.com>
2019-09-27 16:39:11 -0300

    test

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <html>
  <head>
-  <title>PHP Test V31</title>
+  <title>PHP Test V32</title>
  </head>
  <body>
-<h1>PHP Test V31</h1>
+<h1>PHP Test V32</h1>
 <?php phpinfo(); ?> 
  </body>
 </html>
